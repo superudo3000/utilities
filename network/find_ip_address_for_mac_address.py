9d61e71b10bb18f923816aa97514f6765553227e
Jochen Kupperschmidt <homework@nwsnet.de>
2014-07-05 19:28:08 +0200

    Adhere to PEP 8. Use 'format' function instead of '%' operator.

--- network/find_ip_address_for_mac_address.py
+++ network/find_ip_address_for_mac_address.py
@@ -16,7 +16,7 @@ Requires Python_ 2.7 or 3.3.
 .. _Python: http://python.org/
 
 :Copyright: 2014 `Jochen Kupperschmidt <http://homework.nwsnet.de/>`_
-:Date: 25-Jan-2014
+:Date: 05-Jul-2014 (original release: 25-Jan-2014)
 :License: MIT
 """
 
@@ -39,6 +39,7 @@ def scan_for_hosts(ip_range):
     nmap_args = ['nmap', '-n', '-sP', '-oX', '-', ip_range]
     return subprocess.check_output(nmap_args)
 
+
 def find_ip_address_for_mac_address(xml, mac_address):
     """Parse Nmap's XML output, find the host element with the given
     MAC address, and return that host's IP address.
@@ -47,21 +48,27 @@ def find_ip_address_for_mac_address(xml, mac_address):
     host = next(filter(host_has_mac_address, hosts))
     return get_ip_address(host)
 
+
 def host_has_mac_address(host_elem):
     """Return true if the host has the given MAC address."""
     mac_address_found = get_address_of_type(host_elem, 'mac')
     return mac_address_found.lower() == MAC_ADDRESS.lower()
 
+
 def get_ip_address(host_elem):
     """Return the host's IP address."""
     return get_address_of_type(host_elem, 'ipv4')
 
+
 def get_address_of_type(host_elem, type_):
     """Return the host's address of the given type."""
-    return host_elem.find('./address[@addrtype="%s"]' % type_).get('addr')
+    return host_elem \
+        .find('./address[@addrtype="{}"]'.format(type_)) \
+        .get('addr')
+
 
 if __name__ == '__main__':
     xml = scan_for_hosts(IP_RANGE)
     ip_address = find_ip_address_for_mac_address(xml, MAC_ADDRESS)
-    print('Found IP address %s for MAC address %s.'
-        % (ip_address, MAC_ADDRESS))
+    print('Found IP address {} for MAC address {}.'
+          .format(ip_address, MAC_ADDRESS))
