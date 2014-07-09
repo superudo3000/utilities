973e03e8944ab6f65ed710bd49c5e7951caae4be
Jochen Kupperschmidt <homework@nwsnet.de>
2014-07-09 22:41:11 +0200

    Switched from 'optparse' to 'argparse' (bundled with Python 2.7; install separately on earlier version). Extracted methods.

--- biggestfiles.py
+++ biggestfiles.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Find the biggest files.
+"""List the biggest files.
 
-:Copyright: 2008 Jochen Kupperschmidt
-:Date: 30-Jul-2008
+:Copyright: 2008-2014 Jochen Kupperschmidt
+:Date: 09-Jul-2014 (original release: 20-Apr-2008)
 :License: MIT
 """
 
+from argparse import ArgumentParser
 from glob import iglob
 from heapq import heapify, heapreplace
 from itertools import islice
-from optparse import OptionParser
 import os
 
 
@@ -21,14 +21,15 @@ def get_files_info(path, pattern):
         for filename in iglob(os.path.join(root, pattern)):
             yield int(os.path.getsize(filename)), filename
 
+
 def identify_biggest_files(files, limit):
     """Determine the biggest files.
 
     ``files``
         An iterable of ``(size, filename)`` tuples.
     ``limit``
-        The maximum number of files to keep on the heap.  A lower value might
-        result in slightly less memory usage.
+        The maximum number of files to keep on the heap.  A lower value
+        might result in slightly less memory usage.
     """
     # Create the initial heap.
     biggest = list(islice(files, limit))
@@ -43,23 +44,36 @@ def identify_biggest_files(files, limit):
     biggest.sort(reverse=True)
     return list(biggest)
 
-def main():
-    # Utilize an option/argument parser.
-    parser = OptionParser(usage='%prog [options] <path>')
-    parser.add_option('-m', '--max-files', dest='max_files',
-        type='int', default=10,
-        help='maximum number of files to show (default: 10)')
-    parser.add_option('-p', '--pattern', dest='pattern',
-        default='*', help='a pattern to narrow down the search, e.g. "*.txt"\n'
-            ' NOTE: The pattern might need to be escaped, possibly using'
-            ' quotes or backslashes, depending on your shell.')
-    opts, args = parser.parse_args()
-    if len(args) != 1:
-        parser.print_help()
-        parser.exit()
 
-    files = get_files_info(args[0], opts.pattern)
-    biggest_files = identify_biggest_files(files, opts.max_files)
+def parse_args():
+    parser = ArgumentParser(description='List the biggest files.')
+
+    parser.add_argument(
+        'path',
+        metavar='PATH')
+
+    parser.add_argument(
+        '-m', '--max-files',
+        dest='max_files',
+        type=int,
+        default=10,
+        help='maximum number of files to show (default: 10)')
+
+    parser.add_argument(
+        '-p', '--pattern',
+        dest='pattern',
+        default='*',
+        help='a pattern to narrow down the search, e.g. "*.txt"\n'
+             ' NOTE: The pattern might need to be escaped, possibly '
+             'using quotes or backslashes, depending on your shell.')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    files = get_files_info(args.path, args.pattern)
+    biggest_files = identify_biggest_files(files, args.max_files)
 
     # Display biggest files.
     if biggest_files:
@@ -68,6 +82,7 @@ def main():
             print tmpl % file_tuple
     else:
         print 'No files were found.'
+
 
 if __name__ == '__main__':
     main()
